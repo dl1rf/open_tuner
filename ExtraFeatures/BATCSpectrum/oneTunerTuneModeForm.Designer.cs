af2d9be80b0933b6dc72dd492300b415221eedee
Martin, DL1RF <dl1rf@users.noreply.github.com>
2024-11-26 07:44:40 +0100

    Cancel Button added

--- ExtraFeatures/BATCSpectrum/oneTunerTuneModeForm.Designer.cs
+++ ExtraFeatures/BATCSpectrum/oneTunerTuneModeForm.Designer.cs
@@ -33,6 +33,7 @@
             this.tuneMode1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -67,7 +68,9 @@
             this.tuneMode1.FormattingEnabled = true;
             this.tuneMode1.Items.AddRange(new object[] {
             "Manual",
-            "Auto"});
+            "Auto (Hold)",
+            "Auto (Next new)",
+            "Auto (Timed)"});
             this.tuneMode1.Location = new System.Drawing.Point(50, 20);
             this.tuneMode1.Name = "tuneMode1";
             this.tuneMode1.Size = new System.Drawing.Size(121, 21);
@@ -94,11 +97,23 @@
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(111, 61);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 28);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
             // oneTunerTuneModeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(331, 99);
+            this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.groupBox1);
             this.MaximizeBox = false;
@@ -119,5 +134,6 @@
         private System.Windows.Forms.ComboBox tuneMode1;
         private System.Windows.Forms.CheckBox avoidBeacon1;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
     }
 }

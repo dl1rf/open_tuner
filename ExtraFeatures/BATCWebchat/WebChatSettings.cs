cb5e534afba463a8ed1df4f7dfee71d1c94bab50
Martin <dl1rf@users.noreply.github.com>
2025-11-09 17:04:42 +0100

    Update WebChatSettings.cs

--- ExtraFeatures/BATCWebchat/WebChatSettings.cs
+++ ExtraFeatures/BATCWebchat/WebChatSettings.cs
@@ -1,22 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace opentuner.ExtraFeatures.BATCWebchat
+﻿namespace opentuner.ExtraFeatures.BATCWebchat
 {
     public class WebChatSettings
     {
         // chat settings        
-        public string sigreport_template = "SigReport: {SN}/{SP} - {DBM} - ({MER}) - {SR} - {FREQ}";
+        public string sigreport_template = "SigReport: {SN}/{SP} - D{DBM} - ({MER}) - {SR} - {FREQ}";
         public string nickname = "NONICK";
         public int chat_font_size = 12;
-        public int gui_chat_width = 500;
-        public int gui_chat_height = 500;
+        public int gui_chat_width = 800;
+        public int gui_chat_height = 600;
         public int gui_chat_x = 0;
         public int gui_chat_y = 0;
         public int gui_chat_windowstate = 0;
-        public int gui_chat_seperator = 180;
+        public int gui_chat_seperator = 220;
     }
 }

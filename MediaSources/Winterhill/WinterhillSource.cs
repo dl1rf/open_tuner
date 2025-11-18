f3a9a93e82fbfdd6284520f5ccf6ccc0609fe19a
Martin <dl1rf@users.noreply.github.com>
2025-11-18 07:59:31 +0100

    Fix Ping WinterHill / PicoTunerWH at startup

--- MediaSources/Winterhill/WinterhillSource.cs
+++ MediaSources/Winterhill/WinterhillSource.cs
@@ -101,19 +101,26 @@ namespace opentuner.MediaSources.Winterhill
 
             // check if tuner is connectable and get local ip
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
-                try
+            try
+            {
+                if (defaultInterface == 1)
                 {
                     socket.Connect(_settings.WinterhillWSHost, _settings.WinterhillWSUdpBasePort);
-                    IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
-                    _LocalIp = endPoint.Address.ToString();
-                    socket.Close();
                 }
-                catch (Exception ex)
+                else
                 {
-                    Log.Error("Socket Connect Exception: " + ex.Message);
-                    return -2;
+                    socket.Connect(_settings.WinterhillUdpHost, _settings.WinterhillUdpBasePort);
                 }
-                Log.Information("Local IP used " + _LocalIp);
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                _LocalIp = endPoint.Address.ToString();
+                socket.Close();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Socket Connect Exception: " + ex.Message);
+                return -2;
+            }
+            Log.Information("Local IP used " + _LocalIp);
 
             // connect interface
             switch (defaultInterface)
@@ -151,7 +158,6 @@ namespace opentuner.MediaSources.Winterhill
 
                     UDPSetVoltage(0, _settings.LNBVoltage[0]);
                     UDPSetVoltage(1, _settings.LNBVoltage[1]);
-
 
                     ts_devices = 2;
                     hw_device = 2;
